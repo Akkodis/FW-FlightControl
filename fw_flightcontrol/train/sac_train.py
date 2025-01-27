29dcd1449ff21ca3acbb6a6b1cce68f975172dfc
Dobid <davidolivares68@gmail.com>
2025-01-27 14:43:13 +0100

    added ts CAPS coef wandb logging

--- fw_flightcontrol/train/sac_train.py
+++ fw_flightcontrol/train/sac_train.py
@@ -234,6 +234,7 @@ def train(cfg: DictConfig):
                 writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
                 writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
                 writer.add_scalar("losses/alpha", alpha, global_step)
+                writer.add_scalar("losses/ts_loss", ts_loss.item(), global_step)
                 print("SPS:", int(global_step / (time.time() - start_time)))
                 writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
                 if cfg_sac.autotune:
