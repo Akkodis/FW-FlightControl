c69162420fa915d027f0aea7b280469e08b1e915
Dobid <davidolivares68@gmail.com>
2025-01-27 14:47:33 +0100

    sac eval switched to use sac_norm and adapted to new state space of alt tracking (SS3)

--- fw_flightcontrol/eval/altitude_control/sac_eval_alttrack.py
+++ fw_flightcontrol/eval/altitude_control/sac_eval_alttrack.py
@@ -7,7 +7,7 @@ import hydra
 import matplotlib.pyplot as plt
 
 from omegaconf import DictConfig
-from fw_flightcontrol.agents.sac import Actor_SAC
+from fw_flightcontrol.agents.sac_norm import Actor_SAC
 from fw_flightcontrol.utils.train_utils import make_env
 from fw_jsbgym.trim.trim_point import TrimPoint
 
@@ -49,6 +49,9 @@ def eval(cfg: DictConfig):
 
     ep_rewards = [0]
     rolls = [env.unwrapped.sim['attitude/roll-rad']]
+    alts = [env.unwrapped.sim['position/enu-z-m']]
+    enu_xs = [env.unwrapped.sim['position/enu-x-m']]
+    enu_ys = [env.unwrapped.sim['position/enu-y-m']]
     step = 0
     target = np.array([560])
     total_steps = 2000
@@ -61,6 +64,9 @@ def eval(cfg: DictConfig):
         ep_rewards.append(reward)
         obs = torch.Tensor(obs).unsqueeze_(0).to(device)
         rolls.append(env.unwrapped.sim['attitude/roll-rad'])
+        alts.append(env.unwrapped.sim['position/enu-z-m'])
+        enu_xs.append(env.unwrapped.sim['position/enu-x-m'])
+        enu_ys.append(env.unwrapped.sim['position/enu-y-m'])
         done = np.logical_or(terminated, truncated)
 
         if done:
@@ -77,34 +83,37 @@ def eval(cfg: DictConfig):
 
     ep_obss = np.array(ep_obss)
     ep_rewards = np.array(ep_rewards)
-    errs_z = ep_obss[:, 1]
+    enu_xs = np.array(enu_xs)
+    enu_ys = np.array(enu_ys)
+    alts = np.array(alts)
+    errs_z = ep_obss[:, 0]
     dist_to_target = np.sqrt(errs_z**2)
     tsteps = np.linspace(0, ep_obss.shape[0], ep_obss.shape[0])
     fig, ax = plt.subplots(2, 3)
 
-    ax[0,0].plot(tsteps, np.rad2deg(ep_obss[:, 3]))
+    ax[0,0].plot(tsteps, np.rad2deg(ep_obss[:, 1]))
     ax[0,0].set_title("Pitch")
 
-    ax[0,1].plot(tsteps, ep_obss[:, 0])
+    ax[0,1].plot(tsteps, alts)
     ax[0,1].set_title("Altitude")
 
-    ax[0,2].plot(tsteps, ep_obss[:, 4])
+    ax[0,2].plot(tsteps, ep_obss[:, 3])
     ax[0,2].set_title("Airspeed")
     
     # plot the positions in a 3D plot with the line being a gradient of colors between red and purple, red being the first timestep and purple the last
-    # ax[0,2].remove()
-    # ax[0,2] = fig.add_subplot(1,3,3, projection='3d')
+    ax[0,2].remove()
+    ax[0,2] = fig.add_subplot(1,3,3, projection='3d')
     # ax[0,2].plot([target[0]], [target[1]], [target[2]], 'ro')
-    # ax[0,2].set_title("X")
-    # ax[0,2].set_ylabel("Y")
-    # ax[0,2].set_zlabel("Z")
+    ax[0,2].set_title("X")
+    ax[0,2].set_ylabel("Y")
+    ax[0,2].set_zlabel("Z")
     # ax[0,2].set_xlim(-400, 400)
     # ax[0,2].set_ylim(-100, 400)
     # ax[0,2].set_zlim(400, 800)
 
  
-    # for i in range(ep_obss.shape[0] - 1):
-    #     ax[0,2].plot(ep_obss[i:i+2, 0], ep_obss[i:i+2, 1], ep_obss[i:i+2, 2], c=plt.cm.plasma(i/ep_obss.shape[0]))
+    for i in range(ep_obss.shape[0] - 1):
+        ax[0,2].plot(enu_xs[i:i+2], enu_ys[i:i+2], alts[i:i+2], c=plt.cm.plasma(i/enu_xs.shape[0]))
 
     ax[1,0].plot(tsteps, dist_to_target)
     ax[1,0].set_title("Distance to target")
@@ -115,8 +124,8 @@ def eval(cfg: DictConfig):
     # ax[1,1].plot(tsteps, np.rad2deg(rolls))
     # ax[1,1].set_title("Roll")
 
-    ax[1,2].plot(tsteps, ep_obss[:, 10], label="elevator")
-    ax[1,2].plot(tsteps, ep_obss[:, 11], label="throttle")
+    ax[1,2].plot(tsteps, ep_obss[:, 9], label="elevator")
+    ax[1,2].plot(tsteps, ep_obss[:, 10], label="throttle")
     ax[1,2].set_title("Commands")
     ax[1,2].legend()
 
