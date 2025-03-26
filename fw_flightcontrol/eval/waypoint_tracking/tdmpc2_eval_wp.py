da3f18c69459902b17fc0b117f4104a0eca6239a
Dobid <davidolivares68@gmail.com>
2025-03-26 14:25:21 +0100

    to comply with new prepare_targets() method (that handles pid  target generation)

--- fw_flightcontrol/eval/waypoint_tracking/tdmpc2_eval_wp.py
+++ fw_flightcontrol/eval/waypoint_tracking/tdmpc2_eval_wp.py
@@ -70,8 +70,7 @@ def eval(cfg: DictConfig):
     # Load and prepare targets
     targets_np_file = 'eval/waypoint_tracking/targets/target_points360_200m.npy'
     targets_enu = np.load(targets_np_file)
-    # targets_enu = targets_enu[:4, :]
-    targets_ecef = eval_sim.prepare_targets(env, targets_enu, cfg_rl)
+    targets_ecef, _ = eval_sim.prepare_targets(env, targets_enu, cfg_rl)
 
     if cfg_rl.eval.run_eval_sims:
         # Run all simulations
